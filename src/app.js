d9251e46d8c084c18a0bc19a9303ddd1fcf11ea5
Isutomu <isutomu@gmail.com>
2025-01-27 20:34:03 -0300

    Disable helmet temporarily

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const corsOptions = require("./config/corsOptions");
 // General setup
 const app = express();
 app.use(cors());
-app.use(helmet({ crossOriginResourcePolicy: false }));
+// app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
